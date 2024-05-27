5d68533edd5946b4f5ad389ef9cfddad863c4cf0
izonit <daniilvorobyov0501@gmail.com>
2024-05-27 14:18:09 +0400

    Minor changes.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,26 @@
 let easter = document.getElementById("easteregg");  
+let welcome = document.getElementById("welcome");
 
 easter.onclick = function () {
     alert("Click-click! You found it.");
-}
+};
 
-window.scroll({
-    top: 25000,
-    left: 0,
-    behavior: 'smooth'
-});
+function scroll() {
+    window.scroll({
+        top: 25000,
+        left: 0,
+        behavior: 'smooth'
+    });
+    
+    windows.scrollBy({
+        top: 100,
+        left: 0,
+        behavior: 'smooth'
+    });
+    
+    document.querySelector("#more").scrollIntoView({
+        behavior: "smooth",
+    });
+};
 
-windows.scrollBy({
-    top: 100,
-    left: 0,
-    behavior: 'smooth'
-});
-
-document.querySelector("#more").scrollIntoView({
-    behavior: "smooth",
-});
+scroll();
